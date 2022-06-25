cf6188b28564f3b41203bb0a5b04fcf9651c6038
Nicolas Lucero <lucero.nicolas.ezequiel@gmail.com>
2022-06-25 00:57:36 -0300

    PM-0001: Initial Project

---
+++ PortfolioMicroservice/src/main/java/PortfolioMicroservice/PortfolioMicroservice/PortfolioMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package PortfolioMicroservice.PortfolioMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioMicroserviceApplication.class, args);
+	}
+
+}
